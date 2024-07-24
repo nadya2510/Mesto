ce1671cd5d7af57a2e50e14b7674009cfb6dd0f0
Nadya <nadya.nik.ven@yandex.ru>
2024-07-24 10:46:22 +0300

    second commit

--- src/test/java/Mesto1Test.java
+++ src/test/java/Mesto1Test.java
@@ -16,6 +16,15 @@ public class Mesto1Test {
     public void setUp() {
         RestAssured.baseURI = "https://qa-mesto.praktikum-services.ru";
     }
+    @Test
+    @DisplayName("Check user name")
+    @Description("This test is for check current user's name.")
+    public void checkUserName() {
+        given()
+                .auth().oauth2(bearerToken) // Передаём токен для аутентификации
+                .get("/api/users/me") // Делаем GET-запрос
+                .then().assertThat().body("data.name", equalTo("Incorrect Name")); // Проверяем, что имя соответствует ожидаемому
+    }
 
     @Test
     @DisplayName("Add a new photo")
